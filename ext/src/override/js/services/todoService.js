87b03b6f072a534800b1df2cad7e697eade5468d
Sean McGrath <mcgraths@gmail.com>
2015-01-02 06:26:07 -0400

    start of todoService refactor

---
+++ ext/src/override/js/services/todoService.js
@@ -0,0 +1,7 @@
+angular.module('todoModule', [''])
+
+  .factory('todoService', function() {
+	  var shinyNewServiceInstance;
+	  // factory function body that constructs shinyNewServiceInstance
+	  return shinyNewServiceInstance;
+});
