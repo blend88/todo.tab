314d7e2d6ca683c1a03efac1314eb5d22f490373
Sean McGrath <mcgraths@gmail.com>
2015-01-03 21:10:42 -0400

    Improved gruntfile with jshint, concat and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
 var paths = {
-  js: ['*.js', 'ext/src/override/js/*.js', 'ext/src/override/js/**/*.js'],
+  js: {
+    vendors: [
+      'ext/src/override/js/vendor/jquery-1.11.2.min.js',
+      'ext/src/override/js/vendor/angular.min.js',
+      'ext/src/override/js/vendor/angular-sanitize.min.js',
+      'ext/src/override/js/vendor/lodash.min.js',
+      'ext/src/override/js/vendor/marked.min.js',
+    ], 
+    app: ['ext/src/override/js/app.js'], 
+    modules: [
+      'ext/src/override/js/modules/storage.js', 
+      'ext/src/override/js/modules/todo.js'
+    ]
+  },
   html: ['ext/src/override/*.html'],
   css: ['ext/src/override/style/*.scss', 'ext/src/override/style/**/*.scss']
 };
@@ -11,8 +24,8 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       js: {
-        files: paths.js,
-        //tasks: ['jshint'],
+        files: [paths.js.app, paths.js.modules],
+        tasks: ['jshint', 'concat'],
         options: {
           livereload: true
         }
@@ -28,6 +41,34 @@ module.exports = function(grunt) {
         tasks: ['sass:dev'],
         options: {
           livereload: true
+        }
+      }
+    },
+    jshint: {
+      all: [paths.js.app, paths.js.modules]
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      vendors: {
+        src: paths.js.vendors,
+        dest: 'ext/src/override/js/vendor/vendors.js',
+      },
+      modules: {
+        src: paths.js.modules,
+        dest: 'ext/src/override/js/modules/modules.js',
+      },
+      app: {
+        src: paths.js.app,
+        dest: 'ext/src/override/js/app.min.js',
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'ext/src/override/js/modules/modules.js': 'ext/src/override/js/modules/modules.js',
+          'ext/src/override/js/app.min.js': 'ext/src/override/js/app.min.js'
         }
       }
     },
@@ -55,11 +96,11 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   // Default task(s).
-  grunt.registerTask('default', ['sass:dev', 'watch']);
+  grunt.registerTask('default', ['sass:dev', 'concat', 'jshint', 'watch']);
 
 
   //prepare for deployment
-  grunt.registerTask('build', ['sass:dist']);
+  grunt.registerTask('build', ['sass:dist', 'concat', 'uglify']);
 
 
 };
